7bcf5436153e1b3c3ed7602e4f9bfa67c786d379
Frédèrick Franck <frederick_spore@hotmail.com>
2024-05-17 14:02:20 +0000

    switch to black

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -13,4 +14,3 @@ def do_api_thing():
     url = "https://api.scryfall.com/cards/56ebc372-aabd-4174-a943-c7bf59e5028d"
     reply = requests.get(url).text
     return reply
-
